e2cd016e9c25a03f7b859e1bdb4c0e4743fa3f98
Ralf Ramsauer <ralf@ramses-pyramidenbau.de>
2016-05-29 22:13:58 +0200

    Add copyright notice
    Signed-off-by: Ralf Ramsauer <ralf@ramses-pyramidenbau.de>

--- moodlights.h
+++ moodlights.h
@@ -1,3 +1,15 @@
+/*
+ * libhausbus: A RS485 Hausbus library
+ *
+ * Copyright (c) Ralf Ramsauer, 2016
+ *
+ * Authors:
+ *   Ralf Ramsauer <ralf@binary-kitchen.de>
+ *
+ * This work is licensed under the terms of the GNU GPL, version 3.  See
+ * the LICENSE file in the top-level directory.
+ */
+
 #pragma once
 
 #include <array>
